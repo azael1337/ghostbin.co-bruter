c52cf48f8335e89d739b11e57935d52ee00ddb2a
coats1337 <53409082+coats1337@users.noreply.github.com>
2020-07-07 15:10:03 +0200

    indent

--- ghost.py
+++ ghost.py
@@ -2,29 +2,34 @@ import string
 import threading
 import random
 import requests
+import platform
 
 from colorama import Fore, init
 
-init(convert=True) # remove this line if you're a linux user
+if platform.system() != 'Linux':
+    init(convert=True)
 
 def save_url(content):
-  with open('hit.txt', 'a+') as f:
-    f.write(content + '\n')
+    with open('hit.txt', 'a+') as f:
+        f.write(content + '\n')
+
 
 def check_urls():
-  while True:
-    code = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(5))
-    r = requests.get(f'https://ghostbin.co/paste/{code}')
-    if r.status_code == 404:
-      print(f'[{Fore.RED}404{Fore.RESET}] => {r.url}')
-    else:
-      save_url(r.url)
-      print(f'[{Fore.GREEN}{r.status_code}{Fore.RESET}] => {r.url}')
+    while True:
+        code = ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(5))
+        r = requests.get(f'https://ghostbin.co/paste/{code}')
+        if r.status_code == 404:
+            print(f'[{Fore.RED}404{Fore.RESET}] => {r.url}')
+        else:
+            save_url(r.url)
+            print(f'[{Fore.GREEN}{r.status_code}{Fore.RESET}] => {r.url}')
+
 
 def main(threads):
-  if threading.active_count() <= threads:
-    t = threading.Thread(target=check_urls, args=())
-    t.start()
+    if threading.active_count() <= threads:
+        t = threading.Thread(target=check_urls, args=())
+        t.start()
+
 
 thread = int(input("How many threads: "))
 main(thread)
